7e21e51b298ae51b72e923d6afe83637c5461247
Aaditi <aadi2007.aaditi@gmail.com>
2020-09-29 21:12:51 -0700

    add htmlRoutes

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8081;
 const apiRoutes = require('./routes/apiRoutes/notesRoutes');
-//const htmlRoutes = require('./routes/htmlRoutes');
+const htmlRoutes = require('./routes/htmlRoutes/index');
 //const notesArr = [];
 
 
 //console.log(notes);
-//app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
 app.listen(PORT, () => console.log(`Started server on port ${PORT}`));
